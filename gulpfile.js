e4d902915e3a81e32a9417f0274359bb5a9c3272
Debashis Mondal <debashis.mondal@accionlabs.com>
2019-09-27 21:35:02 +0530

    fix(): fulp modification

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,30 @@ var modRewrite = require('connect-modrewrite');
 var scss = require('gulp-sass');
 
 var PORT = process.env.PORT || 5000;
-var UIPORT = 5001;
+var UIPORT = process.env.UIPORT || 5001;
 
 const dirs = {
-    uiPath: path.resolve(__dirname, 'public'),
+    uiPath: process.env.BASE || path.resolve(__dirname, 'public'),
     nodeModules: path.resolve(__dirname, 'node_modules'),
     bowerComponent: path.resolve(__dirname, 'bower_components')
 }
-gulp.task('scss', function () {
+
+function execSCSS() {
     return gulp.src(dirs.uiPath + '/assets/scss/**/*.scss')
         .pipe(scss({ outputStyle: 'compressed' }).on('error', scss.logError))
         .pipe(gulp.dest(dirs.uiPath + '/assets/css'));
-});
+}
 
-gulp.task('watch', () => {
+function watch() {
     gulp.watch(dirs.uiPath + '/**/*', browserSync.reload);
-    gulp.watch(dirs.uiPath + '/assets/scss/**/*.scss', ['scss']);
-});
+    gulp.watch(dirs.uiPath + '/assets/scss/**/*.scss', execSCSS);
+}
 
-gulp.task('browser-sync', () => {
+function liveReload() {
     /**
-    * Gulp browser sync configuration options
-    * Link : https://www.browsersync.io/docs/options
-    */
+        * Gulp browser sync configuration options
+        * Link : https://www.browsersync.io/docs/options
+        */
     browserSync({
         port: PORT,
         ui: {
@@ -49,6 +50,8 @@ gulp.task('browser-sync', () => {
             ],
         }
     });
-});
+}
 
-gulp.task('default', ['browser-sync', 'watch', 'scss']);
+const runDev = gulp.series(liveReload, watch, execSCSS)
+
+exports.default = runDev;
